1bdb66243ae35e57c555a48d2772eeac6398e2de
Rahul <rahulna1996@gmail.com>
2024-01-09 22:12:40 +0530

    Added redux store and cartSlice

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./NavBar";
+import { Provider } from "react-redux";
+import store from "../store/store";
 
 export const RootLayout = () => {
   return (
     <>
-      <NavBar />
-      <main>
-        <Outlet />
-      </main>
+      <Provider store={store}>
+        <NavBar />
+        <main>
+          <Outlet />
+        </main>
+      </Provider>
     </>
   );
 };
